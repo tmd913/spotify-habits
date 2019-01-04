339040939be5a6ac8fdc3b1b0a17de43600a8ac4
Tom Dennis <tdennis913@gmail.com>
2019-01-04 17:19:22 -0500

    initial state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { defaultCipherList } from "constants";
+
+let defaultStyle = {
+  color: "#fff"
+}
+
+const Aggregate = () => (
+  <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
+    <h2>Number Text</h2>
+  </div>
+);
+
+const Filter = () => (
+  <div>
+    <input type="text"/>
+    Filter
+  </div>
+)
+
+const Playlist = () => (
+  <div style={{width: "30%", display: "inline-block"}}>
+    <img src="" alt=""/>
+    <h3>Playlist Name</h3>
+    <ul style={{listStyleType: "none", textAlign: "left"}}>
+      <li>Song 1</li>
+      <li>Song 2</li>
+      <li>Song 3</li>
+    </ul>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={defaultStyle}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 style={{margin: 0, paddingTop: "2vh"}}>Title</h1>
         </header>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
     );
   }
