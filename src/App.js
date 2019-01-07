6df3c7f4bfd8ac1724bda31018c243b704e6d41f
Tom Dennis <tdennis913@gmail.com>
2019-01-07 17:12:51 -0500

    added logic using fake data

--- src/App.js
+++ src/App.js
@@ -5,46 +5,164 @@ import { defaultCipherList } from "constants";
 
 let defaultStyle = {
   color: "#fff"
-}
+};
 
-const Aggregate = () => (
-  <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-    <h2>Number Text</h2>
+let fakeServerData = {
+  user: {
+    name: "Tom",
+    playlists: [
+      {
+        name: "Prime Cuts",
+        songs: [
+          {
+            name: "Nuclear",
+            duration: 1234
+          },
+          {
+            name: "Catch Me",
+            duration: 1234
+          },
+          {
+            name: "Say It",
+            duration: 1234
+          }
+        ]
+      },
+      {
+        name: "Vibes",
+        songs: [
+          {
+            name: "Pacific Coast Highway",
+            duration: 1234
+          },
+          {
+            name: "Last To Leave",
+            duration: 1234
+          },
+          {
+            name: "Better Not",
+            duration: 1234
+          }
+        ]
+      },
+      {
+        name: "Human Music",
+        songs: [
+          {
+            name: "Emily",
+            duration: 1234
+          },
+          {
+            name: "Out Of Love",
+            duration: 1234
+          },
+          {
+            name: "Another Day In Paradise",
+            duration: 1234
+          }
+        ]
+      },
+      {
+        name: "Mix",
+        songs: [
+          {
+            name: "Crawl Outta Love",
+            duration: 1234
+          },
+          {
+            name: "Shake Something",
+            duration: 1234
+          },
+          {
+            name: "Sensations",
+            duration: 1234
+          }
+        ]
+      }
+    ]
+  }
+};
+
+const PlaylistCounter = props => (
+  <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+    <h2>
+      {props.playlists ? `${props.playlists.length} Playlists` : "0 Playlists"}
+    </h2>
   </div>
 );
 
+const HoursCounter = props => {
+  let allSongs = props.playlists.reduce(
+    (songs, playlist) => {
+      return songs.concat(playlist.songs);
+    }
+    , []
+  );
+
+  let totalDuration = allSongs.reduce((sum, song) => {
+    return sum += song.duration;
+  }, 0)
+
+  return (
+    <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+      <h2>{`${Math.round(totalDuration/(60**2))} Hours`}</h2>
+    </div>
+  );
+};
+
 const Filter = () => (
   <div>
-    <input type="text"/>
+    <input type="text" />
     Filter
   </div>
-)
+);
 
-const Playlist = () => (
-  <div style={{width: "30%", display: "inline-block"}}>
-    <img src="" alt=""/>
-    <h3>Playlist Name</h3>
-    <ul style={{listStyleType: "none", textAlign: "left"}}>
-      <li>Song 1</li>
-      <li>Song 2</li>
-      <li>Song 3</li>
+const Playlist = props => (
+  <div style={{ width: "30%", display: "inline-block" }}>
+    <img src="" alt="" />
+    <h3>{props.title}</h3>
+    <ul style={{ listStyleType: "none", textAlign: "left" }}>
+      <li>{props.songs[0].name}</li>
+      <li>{props.songs[1].name}</li>
+      <li>{props.songs[2].name}</li>
     </ul>
   </div>
-)
+);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="App" style={defaultStyle}>
-        <header className="App-header">
-          <h1 style={{margin: 0, paddingTop: "2vh"}}>Title</h1>
-        </header>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ? (
+          <div>
+            <header className="App-header">
+              <h1 style={{ margin: 0, paddingTop: "2vh" }}>
+                {`${this.state.serverData.user.name}'s Playlists`}
+              </h1>
+            </header>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
+            <Filter />
+            {this.state.serverData.user.playlists.map(playlist => (
+              <Playlist title={playlist.name} songs={playlist.songs} />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )}
       </div>
     );
   }
